14009c58489a4b795fbc8eeb387ae4e52c687113
Kevin <kbrowne97@gmail.com>
2022-02-18 14:46:37 -0500

    linting fixes in gameboard.js

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -21,13 +21,14 @@ export default function gameboardFactory() {
 
     const allSunk = () => {
         for (let i = 0; i < 10; i += 1) {
-            for (let j = 0; j < 10; j += 1) 
-            if (typeof board[i][j] === 'object') {
-                if (board[i][j].ship.isSunk() === false) return false;
+            for (let j = 0; j < 10; j += 1) {
+                if (typeof board[i][j] === 'object') {
+                    if (board[i][j].ship.isSunk() === false) return false;
+                }
             }
         }
         return true;
-    }
+    };
 
     const placeShip = (coords, length, allignment) => {
         const [x, y] = coords;
@@ -40,8 +41,8 @@ export default function gameboardFactory() {
             for (let i = 0; i < length; i += 1) {
                 board[x][y + i] = {
                     originCoords: [x, y],
-                    ship: ship,
-                }
+                    ship,
+                };
             }
         }
         if (allignment === 'vertical') {
@@ -53,8 +54,8 @@ export default function gameboardFactory() {
             for (let i = 0; i < length; i += 1) {
                 board[x + i][y] = {
                     originCoords: [x, y],
-                    ship: ship,
-                }
+                    ship,
+                };
             }
         }
 
@@ -65,15 +66,15 @@ export default function gameboardFactory() {
         const [x, y] = coords;
         if (isShip(coords)) {
             // get distance between the ship position hit and its origin then pass it to hit()
-            let origin = board[x][y].originCoords;
-            let diff = (x - origin[0]) + (y - origin[0])
-            if (diff < 0) diff = diff * -1;
+            const origin = board[x][y].originCoords;
+            let diff = (x - origin[0]) + (y - origin[0]);
+            if (diff < 0) diff *= -1;
             board[x][y].ship.hit(diff);
         }
         if (!isShip(coords)) {
             board[x][y] = 1;
         }
-    }
+    };
 
     return {
         getBoard,
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -4,7 +4,7 @@ describe('Initiating the board', () => {
     it('Creates a 10 x 10 board array', () => {
         const board1 = gameboardFactory();
         expect(board1.getBoard()).toStrictEqual(
-                [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,10 +13,10 @@ describe('Initiating the board', () => {
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
         );
     });
-})
+});
 
 describe('Placing ships', () => {
     it('Places ships in the correct horizontal position', () => {
@@ -27,15 +27,14 @@ describe('Placing ships', () => {
         expect(board1.isShip([0, 2])).toBe(true);
         expect(board1.isShip([0, 3])).toBe(false);
         expect(board1.isShip([1, 0])).toBe(false);
-    
+
         board1.placeShip([3, 4], 2, 'horizontal');
         expect(board1.isShip([3, 4])).toBe(true);
         expect(board1.isShip([3, 5])).toBe(true);
         expect(board1.isShip([3, 6])).toBe(false);
         expect(board1.isShip([3, 3])).toBe(false);
-    
     });
-    
+
     it('Places ships in the correct vertical position', () => {
         const board1 = gameboardFactory();
         board1.placeShip([0, 0], 3, 'vertical');
@@ -44,15 +43,14 @@ describe('Placing ships', () => {
         expect(board1.isShip([2, 0])).toBe(true);
         expect(board1.isShip([0, 1])).toBe(false);
         expect(board1.isShip([1, 1])).toBe(false);
-    
-    })
-    
+    });
+
     it('Does not place ships out of bounds', () => {
         const board1 = gameboardFactory();
         expect(board1.placeShip([0, 9], 2, 'horizontal')).toBe('error: out of bounds');
         expect(board1.placeShip([9, 1], 2, 'vertical')).toBe('error: out of bounds');
-    })
-    
+    });
+
     it('Does not place ships on occupied cells', () => {
         const board1 = gameboardFactory();
         board1.placeShip([0, 0], 4, 'horizontal');
@@ -60,30 +58,30 @@ describe('Placing ships', () => {
         expect(board1.placeShip([0, 3], 2, 'horizontal')).toBe('error: occupied by ship');
         expect(board1.placeShip([0, 3], 2, 'vertical')).toBe('error: occupied by ship');
         expect(board1.placeShip([4, 2], 2, 'horizontal')).toBe('error: occupied by ship');
-    })
-    
+    });
+
     it('All ship cells of the same ship reference the same ship object', () => {
         const board1 = gameboardFactory();
         board1.placeShip([0, 0], 3, 'horizontal');
         board1.placeShip([3, 3], 4, 'horizontal');
         expect(board1.cellStatus([0, 0]).ship).toBe(board1.cellStatus([0, 1]).ship);
         expect(board1.cellStatus([3, 3]).ship === board1.cellStatus([0, 0]).ship).toBe(false);
-    })
-    
+    });
+
     it('Unique ship cells reference separate ship objects', () => {
         const board1 = gameboardFactory();
         board1.placeShip([0, 0], 3, 'horizontal');
         board1.placeShip([3, 3], 4, 'horizontal');
-        expect(board1.cellStatus[3, 3] === board1.cellStatus([0, 0])).toBe(false);
-    })        
-})
+        expect(board1.cellStatus([3, 3]) === board1.cellStatus([0, 0])).toBe(false);
+    });
+});
 
 describe('Recieving hits', () => {
     it('Marks water spaces as 1', () => {
         const board1 = gameboardFactory();
         board1.recieveAttack([0, 0]);
         expect(board1.cellStatus([0, 0])).toBe(1);
-    })
+    });
 
     it('Marks correct horizontal ship position as hit', () => {
         const board1 = gameboardFactory();
@@ -93,8 +91,7 @@ describe('Recieving hits', () => {
         expect(board1.cellStatus([0, 0]).ship.isHit(1)).toBe(false);
         board1.recieveAttack([0, 1]);
         expect(board1.cellStatus([0, 0]).ship.isHit(1)).toBe(true);
-
-    })
+    });
 
     it('Marks correct vertical ship position as hit', () => {
         const board1 = gameboardFactory();
@@ -104,8 +101,8 @@ describe('Recieving hits', () => {
         expect(board1.cellStatus([0, 0]).ship.isHit(1)).toBe(false);
         board1.recieveAttack([1, 0]);
         expect(board1.cellStatus([0, 0]).ship.isHit(1)).toBe(true);
-    })
-})
+    });
+});
 
 describe('Are all ships sunk?', () => {
     it('Checks if all ships are sunk', () => {
@@ -116,5 +113,6 @@ describe('Are all ships sunk?', () => {
         board1.recieveAttack([0, 0]);
         board1.recieveAttack([0, 1]);
         expect(board1.allSunk()).toBe(true);
-    })
-})
+        board1.placeShip([0, 3, 4, 'vertical']);
+    });
+});
